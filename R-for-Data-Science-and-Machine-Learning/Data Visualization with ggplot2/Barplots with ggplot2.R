6a29b7ecad1e2d8bb8b2cab92cdb02c31521a026
vozret <vozret00@fesb.hr>
2022-03-17 11:59:35 +0100

    barplots

---
+++ R-for-Data-Science-and-Machine-Learning/Data Visualization with ggplot2/Barplots with ggplot2.R
@@ -0,0 +1,19 @@
+# count when dealing with categorical data
+
+library(ggplot2)
+df <- mpg
+head(mpg)
+
+# data and aesthetics
+pl <- ggplot(df, aes(x=class))
+print(pl + geom_bar())
+
+# bar_values for third value of data
+
+print(pl + geom_bar(color="blue", fill="blue"))
+
+# automatically create autostacked barplot
+print(pl + geom_bar(aes(fill=drv)))
+
+print(pl + geom_bar(aes(fill=drv), position="dodge"))
+
