3a235fe8449269b73026ccb7f4a91681846f2b8d
vozret <vozret00@fesb.hr>
2022-04-11 10:12:44 +0200

    linear regression part 2, building a  model

--- R-for-Data-Science-and-Machine-Learning/Machine Learning with R/Linear Regression.R
+++ R-for-Data-Science-and-Machine-Learning/Machine Learning with R/Linear Regression.R
@@ -50,16 +50,50 @@ corrgram(df, order=TRUE, lower.panel = panel.shade,
 print(ggplot(df, aes(x=G3)) + geom_histogram(bins=20, alpha = 0.5, fill = "blue"))
 # high mean value at 10
 
+print(ggplot(df, aes(x=G3)) + geom_histogram(bins=40, alpha = 0.5, fill = "blue"))
+
 #### PART 2
 #### BUILDING A MODEL
 
+# split data into train and test set
+# install.packages('caTools')
 
+library(caTools)
 
+# random split
+set.seed(101)
 
+# slit up sample
+# creates new column in the df with random TRUE, FALSE values
+# for train, test split
+sample <- sample.split(df$G3, SplitRatio = 0.7)
 
+# 70% of the data goes into training
+train <- subset(df, sample == TRUE)
+test <- subset(df, sample == FALSE)
 
+# training our model
+# lm - linear model
+# y - feature we are trying to predict
+# x1 + x2 - predictors we want to use
+# data - data
+# model <- lm(y ~ x1 + x2 , data)
+# model <- lm(y ~. , data) # using all features
 
+### BUILDING A MODEL
+model <- lm(G3 ~ ., data = train)
 
+# RUN THE MODEL
+print(summary(model))
+
+## plotting the residuals
+res <- residuals(model)
+class(res)
+
+res <- as.data.frame(res)
+head(res)
+
+print(ggplot(res, aes(res)) + geom_histogram(fill = "blue", alpha = 0.5))
 
 
 
